e8d7d9395ac5890e4b73483b01b97cd7b05481b9
Henry <henryhein.hein@gmail.com>
2023-03-05 02:05:38 -0800

    final build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+//import { useState, useEffect, useRef } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// const App = () => {
 
-export default App;
+//     const usePreviousValue = value => {
+//       const ref = useRef();
+//       useEffect(() => {
+//         ref.current = value;
+//       },[value]);
+//       return ref.current;
+//     };
+
+//     const  [incomingDataR100, setIncomingDataR100] = useState([]);
+//     // const  [incomingDataR50, setIncomingDataR50] = useState([]);
+//     const prevValue = usePreviousValue(incomingDataR100);
+//     const endpoint = 'wss://ws.binaryws.com/websockets/v3?app_id=32978';
+//     var ws = new WebSocket(endpoint);
+    
+    
+//     useEffect(() => {
+//       ws.onopen = (event) => {
+//         console.log('Socket is open')
+//         ws.send(JSON.stringify({ticks:'R_100'}));
+       
+//         // ws.send(JSON.stringify({ticks:'R_50'}));
+//       };
+//     }, [])
+
+//     useEffect (() => {
+//         setInterval(() => {
+//           ws.onmessage = function (event) {
+//             const json = JSON.parse(event.data);
+//             console.log(`Socket is receiving ${json.tick.symbol}`)
+//             if(json.tick.symbol === 'R_100') {
+//               setIncomingDataR100(json.tick.quote);
+//             }
+//             // } else {
+//             //   setIncomingDataR50(json.tick.quote);
+//             // }          
+//           };
+//         }, 1000);
+        
+//     },[incomingDataR100]);
+//     //  incomingDataR50]);
+
+//     const chooseColor = (incomingDataR100, prevValue) => {
+//       if(prevValue > incomingDataR100) {
+//         return 'red';
+//       } else if (incomingDataR100 === prevValue){
+//         return 'black';
+//       } else {
+//         return 'green';
+//       }
+//     }
+//     const chosenColor = chooseColor(incomingDataR100, prevValue);
+
+//     // console.log(incomingDataR100, prevValue);
+//   return (
+//     <div className="App">
+//         <h1>hello world</h1>
+//         <h1>application id: 	32978</h1>
+//         {/* {incomingDataR100.map((item) => (
+//           <>
+//             <li value={item.ask}>{item.ask}</li>
+//           </>
+//         ))}
+
+//         {incomingDataR50.map((item) => (
+//           <>
+//             <li value={item.ask}>{item.ask}</li>
+//           </>
+//         ))} */}
+
+//         <h1 className={chosenColor}>  
+//           {incomingDataR100}
+//         </h1>
+//         <h1>
+//           Previous Value: {prevValue}
+//         </h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
